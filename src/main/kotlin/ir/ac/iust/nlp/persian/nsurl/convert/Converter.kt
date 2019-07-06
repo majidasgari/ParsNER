8ad24370f633035b35f70635759b7125ae7d0949
Majid Asgari <majid.asgari@gmail.com>
2019-07-06 16:36:48 +0430

    NSURL data

---
+++ src/main/kotlin/ir/ac/iust/nlp/persian/nsurl/convert/Converter.kt
@@ -0,0 +1,22 @@
+package ir.ac.iust.nlp.persian.nsurl.convert
+
+import ir.ac.iust.nlp.persian.parser.PathWalker
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.attribute.BasicFileAttributes
+
+fun main() {
+    val pathes = PathWalker.getPath(Paths.get("300K"))
+    pathes.addAll(PathWalker.getPath(Paths.get("600K")))
+    pathes.sortBy { Files.readAttributes(it, BasicFileAttributes::class.java).creationTime() }
+    val results = mutableListOf<String>()
+    pathes.forEach { path ->
+        val lines = Files.readAllLines(path, charset("UTF-8"))
+        if (lines.isNotEmpty()) {
+            results.addAll(lines)
+            results.add("")
+            results.add("")
+        }
+    }
+    Files.write(Paths.get("train.conll"), results, charset("UTF-8"))
+}
